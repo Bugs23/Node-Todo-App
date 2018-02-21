2f3a275d04cd419db5a5df3499cff30953193c0d
EC2 Default User <ec2-user@ip-172-31-11-59.ec2.internal>
2018-02-21 01:37:36 +0000

    Set up files and directories to serve static files

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ var todoRoutes = require('./routes/todos');
 // Allow us to access request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// Serve everything in public directory as static file
+app.use(express.static(__dirname + '/public'));
+// Serve everything in views directory as static file
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
-    res.send('Hello from the root route');
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
@@ -22,4 +26,4 @@ app.use('/api/todos', todoRoutes);
 // Start server     
 app.listen(port, function() {
     console.log("APP IS RUNNING ON PORT " + process.env.PORT);
-})
+});
