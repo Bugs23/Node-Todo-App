64f2ba194d66146989b81106dafff1a4aac4b43a
EC2 Default User <ec2-user@ip-172-31-11-59.ec2.internal>
2018-02-22 00:36:35 +0000

    Make initial ajax call and add items to list

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-alert("hi from app js")
+/* global $ */
+$(document).ready(function() {
+    // Get data from todos
+    $.getJSON("/api/todos")
+    .then(function(addTodos)
+});
+
+// Add todos to page
+function addTodos(todos) {
+    // Loop through each todo
+    todos.forEach(function(todo) {
+        // Create new list item with the name of the todo
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        // Add new item to the list
+        $('.list').append(newTodo);
+    });
+};
