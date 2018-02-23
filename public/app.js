ca31aaac3efbf2b607b37437ac44b0ce3d0e509c
EC2 Default User <ec2-user@ip-172-31-11-59.ec2.internal>
2018-02-23 02:20:33 +0000

    Add functionality to create a todo item

--- public/app.js
+++ public/app.js
@@ -2,16 +2,51 @@
 $(document).ready(function() {
     // Get data from todos
     $.getJSON("/api/todos")
-    .then(function(addTodos)
+    // Add the todos to the page
+    .then(addTodos)
+    
+    // When a button on the keyboard is pressed
+    $('#todoInput').keypress(function(e) {
+        // If the button pressed is the enter key
+        if (e.which == 13) {
+            // Call createTodo function
+            createTodo();
+        }
+    })
 });
 
-// Add todos to page
+// Loop through each todo
 function addTodos(todos) {
-    // Loop through each todo
     todos.forEach(function(todo) {
-        // Create new list item with the name of the todo
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
-        // Add new item to the list
-        $('.list').append(newTodo);
+        // Call addTodo function to add them to the page
+        addTodo(todo);
     });
 };
+
+// Add todos to the list
+function addTodo(todo) {
+    // Create a new todo with the value from the name property
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    // If the todo is completed
+    if (todo.completed) {
+        // Add the done class to the todo
+        newTodo.addClass('done');
+    }
+    // Add the todo to the list of todos
+    $('.list').append(newTodo);
+    
+}
+
+// Send request to create new todo
+function createTodo() {
+    // Get the new todo text from the input field
+    var usrInput = $('#todoInput').val();
+    // Set name property to what the user entered (usrInput) and add it to the database
+    $.post('/api/todos', {name: usrInput})
+    .then(function(newTodo) {
+        addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
